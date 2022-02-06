9a5aa3387419a97a3fc5bdd2e0fe0871d28a40c9
nbn6 <87109055+nbn6@users.noreply.github.com>
2022-02-06 15:05:40 +0800

    Update 2.js

--- 2.js
+++ 2.js
@@ -1,2 +1,18 @@
 [rewrite_local]
+
+#panda widget解锁会员
 ^https:\/\/mobile-api\.adguard\.com\/api/1.0/ios_validate_receipt url script-response-body https://raw.githubusercontent.com/nbn6/2/main/2.js
+[mitm]
+hostname = mobile-api.adguard.com
+
+*/
+
+let obj = JSON.parse($response.body);
+obj = {
+  "products": [{
+    "product_id": "com.adguard.lifetimePurchase",
+    "premium_status": "ACTIVE"
+  }]
+}
+
+$done({body: JSON.stringify(obj)});
