b8a99e93323cfbf1ef470f3db006c4d74b71b8ec
nbn6 <87109055+nbn6@users.noreply.github.com>
2022-02-06 14:58:39 +0800

    Update 2.js

--- 2.js
+++ 2.js
@@ -1,16 +1,2 @@
 [rewrite_local]
 ^https:\/\/mobile-api\.adguard\.com\/api/1.0/ios_validate_receipt url script-response-body https://raw.githubusercontent.com/nbn6/2/main/2.js
-[mitm]
-hostname = mobile-api.adguard.com
-
-*/
-
-let obj = JSON.parse($response.body);
-obj = {
-  "products": [{
-    "product_id": "com.adguard.lifetimePurchase",
-    "premium_status": "ACTIVE"
-  }]
-}
-
-$done({body: JSON.stringify(obj)});
