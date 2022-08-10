5f09af38c606d72fa6a60a26727ce6ebc608b3a7
nbn6 <87109055+nbn6@users.noreply.github.com>
2022-08-10 16:55:33 +0800

    Update Capture2.js

--- Capture2.js
+++ Capture2.js
@@ -4,9 +4,8 @@
 [MITM]
 hostname = -cl5.apple.com, -mzstorekit-sb.itunes.apple.com, -gsas.apple.com, -csig.apple.com, -sandbox.itunes.apple.com, -guzzoni.apple.com, -itunes.apple.com, -musicstatus.itunes.apple.com, -*.mzstatic.com, -xp.apple.com, -*.siri.apple.com, -*.push.apple.com, -amp-api.apps.apple.com, -api-edge.apps.apple.com, -play.itunes.apple.com, -search.apple.com, -amp-api-search-edge.apps.apple.apple.com, -gsa.apple.com, -mzstorekit.itunes.
 
-
-[小组]
+[Panel]
 capture = title = "HTTP Capture", content = "DISABLE", icon = "doc.text.magnifyingglass", icon-color = "FF6D1F", script-name = HTTP-Capture, update-interval = 1
 
-[脚本]
+[Script]
 HTTP-Capture = type=generic, script-path = https://raw.githubusercontent.com/nbn6/2/main/Capture.js?raw=true
